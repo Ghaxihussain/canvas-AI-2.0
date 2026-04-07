f1da503a6e0450e7bad6a301b84b0963fa448aa3
ghazi hussain <ghazihussainsyed721@gmail.com>
2026-04-06 22:57:13 -0500

    add new files

---
+++ Backend/services/aws.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import os
+import boto3
+
+load_dotenv()
+
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("AWS_REGION")
+)
+
+s3.upload_file("test.txt", os.getenv("AWS_BUCKET_NAME"), "test/test.txt")
